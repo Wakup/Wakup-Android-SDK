57b952751224301521092da20e1c44638ae13447
Alex Gutiérrez <alejandro.gutierrez@twincoders.com>
2016-09-05 17:59:55 +0200

    Included custom design for offer tag buttons

--- sdk/src/main/java/com/yellowpineapple/wakup/sdk/views/OfferTagsView.java
+++ sdk/src/main/java/com/yellowpineapple/wakup/sdk/views/OfferTagsView.java
@@ -3,15 +3,17 @@ package com.yellowpineapple.wakup.sdk.views;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.yellowpineapple.wakup.sdk.R;
 import com.yellowpineapple.wakup.sdk.models.Offer;
+
+import org.apmem.tools.layouts.FlowLayout;
 
 /**
  * Created by agutierrez on 30/8/16.
  */
-public class OfferTagsView extends LinearLayout {
+public class OfferTagsView extends FlowLayout {
 
     public interface OnTagSelectedListener {
         void onTagSelected(String tag);
@@ -42,9 +44,15 @@ public class OfferTagsView extends LinearLayout {
     public void setOffer(Offer offer) {
         this.offer = offer;
         removeAllViews();
+        int padding = getResources().getDimensionPixelOffset(R.dimen.wk_tags_padding);
+        setPadding(padding, padding, padding, padding);
         for (final String tag: offer.getTags()) {
-            Button tagButton = new Button(getContext());
+            TextView tagButton = new TextView(getContext());
+            tagButton.setBackgroundResource(R.drawable.wk_tag_bg);
+            tagButton.setTextColor(getResources().getColor(R.color.wk_tag_text));
             tagButton.setText(String.format("#%s", tag));
+            tagButton.setAllCaps(false);
+            tagButton.setClickable(true);
             tagButton.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -53,7 +61,10 @@ public class OfferTagsView extends LinearLayout {
                     }
                 }
             });
-            addView(tagButton);
+            LayoutParams layoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(padding, padding, padding, padding);
+
+            addView(tagButton, layoutParams);
         }
     }
 
