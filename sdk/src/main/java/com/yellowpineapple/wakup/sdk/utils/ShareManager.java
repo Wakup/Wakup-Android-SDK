cd098acc1750cb034936538ab9b5a1f11169f5c0
Alex Gutiérrez <alejandro.gutierrez@twincoders.com>
2019-09-20 11:52:54 +0200

    Fixed offer image sharing

--- sdk/src/main/java/com/yellowpineapple/wakup/sdk/utils/ShareManager.java
+++ sdk/src/main/java/com/yellowpineapple/wakup/sdk/utils/ShareManager.java
@@ -1,9 +1,11 @@
 package com.yellowpineapple.wakup.sdk.utils;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
+import android.provider.MediaStore;
 
 import com.yellowpineapple.wakup.sdk.R;
 import com.yellowpineapple.wakup.sdk.activities.ParentActivity;
@@ -18,7 +20,7 @@ import java.io.IOException;
 public class ShareManager {
 
     public static void shareImage(ParentActivity context, Bitmap image, String fileName, String shareDialogTitle, String text) {
-        Uri bmpUri = getBitmapUri(image, fileName);
+        Uri bmpUri = getBitmapUri(context, image, fileName);
         if (bmpUri != null) {
             // Construct a ShareIntent with link to image
             Intent shareIntent = new Intent();
@@ -26,6 +28,7 @@ public class ShareManager {
             shareIntent.putExtra(Intent.EXTRA_TEXT, text);
             shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
             shareIntent.setType("image/*");
+            shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
             // Launch sharing dialog for image
             context.startActivity(Intent.createChooser(shareIntent, shareDialogTitle));
         } else {
@@ -33,20 +36,9 @@ public class ShareManager {
         }
     }
 
-    private static Uri getBitmapUri(Bitmap bitmap, String fileName) {
+    private static Uri getBitmapUri(Context context, Bitmap bitmap, String fileName) {
         // Store image to default external storage directory
-        Uri bmpUri = null;
-        try {
-            File file =  new File(Environment.getExternalStoragePublicDirectory(
-                    Environment.DIRECTORY_DOWNLOADS), fileName);
-            file.getParentFile().mkdirs();
-            FileOutputStream out = new FileOutputStream(file);
-            bitmap.compress(Bitmap.CompressFormat.PNG, 90, out);
-            out.close();
-            bmpUri = Uri.fromFile(file);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return bmpUri;
+        String bitmapPath = MediaStore.Images.Media.insertImage(context.getContentResolver(), bitmap,fileName, null);
+        return Uri.parse(bitmapPath);
     }
 }
